0729854789946c350171893a83007fa52243bcf0
Unknown <marian.me96@gmail.com>
2018-05-28 13:44:32 +0200

    meh

---
+++ ARDU_GUI/ardu_gui/ardu_gui/dialog.h
@@ -0,0 +1,39 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QtSerialPort/QSerialPort>
+#include <QByteArray>
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = 0);
+    ~Dialog();
+
+private slots:
+    void readSerial();
+    void updateIRval(const QString);
+    void updateVoltage(const QString);
+
+private:
+    Ui::Dialog *ui;
+
+    QSerialPort *arduino;
+    static const quint16 arduino_uno_vendor_id = 10755;
+    static const quint16 arduino_uno_product_id = 67;
+    QByteArray serialData;
+    QString serialBuffer;
+    QString parsed_data;
+    double windmill_val;
+    double supercaps_val;
+
+};
+
+#endif // DIALOG_H
