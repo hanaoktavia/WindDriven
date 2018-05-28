3f6e7cad5e164d3d750ca90f8a3f18c7122a6f66
Unknown <marian.me96@gmail.com>
2018-05-28 11:35:49 +0200

    code_completed

--- 2digitalInput_test/2digital_input/2digital_input.ino
+++ 2digitalInput_test/2digital_input/2digital_input.ino
@@ -1,9 +1,30 @@
+#define pin_with_pullup 8 //overvoltage
+#define pin_with_pullup2 9 //overheating
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pin_with_pullup, INPUT_PULLUP);
+  pinMode(pin_with_pullup2, INPUT_PULLUP);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int sensorVal = digitalRead(pin_with_pullup);
+  int sensorVal2 = digitalRead(pin_with_pullup2);
+
+  //Debug code
+  Serial.print("Debug --> Digitalpin1 value -->");
+  Serial.println(sensorVal);
+  Serial.print("Debug --> Digitalpin2 value -->");
+  Serial.println(sensorVal2);
+
+  if(sensorVal == HIGH)
+  {
+    Serial.print("ATTENTION! OVERVOLTAGE!!");
+  }else{;}
+  if(sensorVal2 == HIGH)
+  {
+    Serial.print("ATTENTION! OVERHEATING!!");
+  }else{;}
 
 }
